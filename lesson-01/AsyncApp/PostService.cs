a17185501e620141747f83f820b38a27341b3b0f
Vadim Sabin <pofoq@mail.ru>
2021-06-17 22:43:13 +0300

    Async Console App

---
+++ lesson-01/AsyncApp/PostService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AsyncApp
+{
+    public static class PostService
+    {
+        private const string _getPostRequestUri = "https://jsonplaceholder.typicode.com/posts/";
+        private static readonly HttpClient _client = new HttpClient();
+
+        public static async Task<Post> GetPost(int id)
+        {
+            try
+            {
+                var response = await _client.GetAsync($"{_getPostRequestUri}{id}");
+                var result = await response.Content.ReadAsStringAsync();
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                var post = JsonSerializer.Deserialize<Post>(result, options);
+                return post;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static async Task<List<Post>> GetPosts(params int[] ids)
+        {
+            var tasks = ids.Select(id => GetPost(id)).ToArray();
+
+            await Task.WhenAll(tasks);
+
+            var posts = tasks
+                .Select(t => t.Result)
+                .Where(p => p != null)
+                .Where(p => p.Id > 0)
+                .ToList();
+
+            return posts;
+        }
+    }
+}
---
+++ lesson-01/AsyncApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Diagnostics;
+
+namespace AsyncApp
+{
+    class Program
+    {
+        public static readonly string FilePath = @"c:\temp\posts.txt";
+        public static readonly int FromId = 4;
+        public static readonly int CountIds = 10;
+
+        static async Task Main(string[] args)
+        {
+            if (File.Exists(FilePath))
+            {
+                File.Delete(FilePath);
+            }
+
+            var ids = Enumerable.Range(FromId, CountIds);
+
+            var posts = await PostService.GetPosts(ids.ToArray());
+
+            Console.WriteLine($"{posts.Count} of {CountIds} posts were got.");
+
+            posts.ForEach(p => p.WriteToFileAsync(FilePath));
+
+            Console.WriteLine("All Done");
+
+            try
+            {
+                Process.Start("notepad.exe", FilePath);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Couldn't open the file ({FilePath})");
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+    }
+}
